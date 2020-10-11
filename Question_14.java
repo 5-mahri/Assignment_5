b2bd9f951b2ef237a9a98b4c2ffd6e79fcc8712a
5-mahri <71532978+5-mahri@users.noreply.github.com>
2020-10-11 23:51:27 +0200

    Add files via upload

---
+++ Question_14.java
@@ -0,0 +1,17 @@
+package assignment5;
+
+public class Question_14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int row=1; row<=7; row++) {
+			
+			System.out.print("#");
+			for(int i=1; i<=row; i++) {
+				System.out.print(" ");
+			}
+			System.out.println("#");
+	}
+	}
+}
