2f50f8990b4371ca1f61ca3d8760044e7defd992
5-mahri <71532978+5-mahri@users.noreply.github.com>
2020-10-11 23:48:11 +0200

    Add files via upload

---
+++ Question_7.java
@@ -0,0 +1,16 @@
+package assignment5;
+
+public class Question_7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=1; i<=100; i++) {
+			if(i%5 == 0) {
+				System.out.print(i+" ");
+			}
+		}
+
+	}
+
+}
