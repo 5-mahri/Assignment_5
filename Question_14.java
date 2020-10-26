aef483fc78759d8360fc30bc613a8a16236df25b
5-mahri <71532978+5-mahri@users.noreply.github.com>
2020-10-26 15:12:18 +0100

    Add files via upload

--- Question_14.java
+++ Question_14.java
@@ -5,13 +5,15 @@ public class Question_14 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		for (int row=1; row<=7; row++) {
-			
+		for (int row=1; row<7; row++) {
 			System.out.print("#");
-			for(int i=1; i<=row; i++) {
+			
+			for(int i=0; i<row-1; i++) {
 				System.out.print(" ");
 			}
 			System.out.println("#");
+			
+			
 	}
 	}
 }
